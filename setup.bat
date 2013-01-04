0f2d41f8f41f4c07db5511efd4b2dbcb4bddd3f3
Nick Andryshak <nick.lanshack@gmail.com>
2013-01-04 14:17:48 -0500

    Moved Stuff

--- setup.bat
+++ setup.bat
@@ -1,7 +1,7 @@
-mkdir %HOMEPATH%\.vim
-mkdir %HOMEPATH%\.vim\bundle
-mkdir %HOMEPATH%\.vim\CustomSnippets
-mklink /H %HOMEPATH%\.vimrc %HOMEPATH%\GitHub\dotfiles\.vimrc
-mklink /H %HOMEPATH%\.gvimrc %HOMEPATH%\GitHub\dotfiles\.gvimrc
-mklink /H %HOMEPATH%\.vim\bundles.vim %HOMEPATH%\GitHub\dotfiles\.vim\bundles.vim
-mklink /H %HOMEPATH%\.vim\CustomSnippets\html.snippets %HOMEPATH%\GitHub\dotfiles\.vim\CustomSnippets\html.snippets
+mkdir %USERPROFILE%\.vim
+mkdir %USERPROFILE%\.vim\bundle
+mkdir %USERPROFILE%\.vim\CustomSnippets
+mklink /H %USERPROFILE%\.vimrc %USERPROFILE%\Documents\GitHub\dotfiles\.vimrc
+mklink /H %USERPROFILE%\.gvimrc %USERPROFILE%\Documents\GitHub\dotfiles\.gvimrc
+mklink /H %USERPROFILE%\.vim\bundles.vim %USERPROFILE%\Documents\GitHub\dotfiles\.vim\bundles.vim
+mklink /H %USERPROFILE%\.vim\CustomSnippets\html.snippets %USERPROFILE%\Documents\GitHub\dotfiles\.vim\CustomSnippets\html.snippets
