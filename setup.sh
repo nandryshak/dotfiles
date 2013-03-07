a2c1a8c704f9b7b08bbbe699eed0036428e45292
Nick Andryshak <nick.lanshack@gmail.com>
2013-03-07 15:08:18 -0500

    linux setup comments

--- setup.sh
+++ setup.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+# Makes directories
 mkdir ~/.vim
 mkdir ~/.vim/bundle
 mkdir ~/.vim/undo
 mkdir ~/.vim/backups
 
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-
+# Hard links vim configs
 ln ~/dotfiles/.vimrc ~/.vimrc
 ln ~/dotfiles/.vim/functions.vim ~/.vim/functions.vim
 ln ~/dotfiles/.vim/bundles.vim ~/.vim/bundles.vim
 
+# Clones vundle
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
+# Installs plugins
 vim +BundleInstall +qall
+
+# Clones snippets
+git clone https://github.com/nandryshak/UltiSnips.git ~/.vim/bundle/ultisnips
